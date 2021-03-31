74e0c65f4fd81d35ed46755c91b84fd98143c303
Syahrul <12050112067@students.uin-suska.ac.id>
2021-03-31 21:51:30 +0700

    Project

---
+++ src/ExceptionListsThreadsFiles/arul/HasMap2.java
@@ -0,0 +1,13 @@
+package ExceptionListsThreadsFiles.arul;
+import java.util.HashMap;
+public class HasMap2 {
+    class A {
+        public void main(String[] args) {
+            HashMap<String, String> m = new HashMap<String, String>();
+            m.put("A","First");
+            m.put("A","Second");
+
+            System.out.println(m.get("B"));
+        }
+    }
+}
