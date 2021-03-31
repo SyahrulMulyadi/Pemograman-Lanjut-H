94c4d1851016a2834629c2a1f02a79ea03d98ff5
Syahrul <12050112067@students.uin-suska.ac.id>
2021-03-31 22:02:59 +0700

    Project

---
+++ src/ExceptionListsThreadsFiles/arul/SortingList1.java
@@ -0,0 +1,18 @@
+package ExceptionListsThreadsFiles.arul;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class SortingList1 {
+    class A {
+        public void main(String[] args) {
+            List<String> list = new ArrayList<String>();
+            list.add("b");
+            list.add("a");
+            list.add("c");
+            Collections.sort(list);
+            System.out.println(list.get(0));
+        }
+    }
+}
