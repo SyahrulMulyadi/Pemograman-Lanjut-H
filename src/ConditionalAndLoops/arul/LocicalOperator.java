ae5a9ef26c2f23d54d35b3793898328e75cea193
Syahrul <12050112067@students.uin-suska.ac.id>
2021-03-31 20:52:21 +0700

    Project

---
+++ src/ConditionalAndLoops/arul/LocicalOperator.java
@@ -0,0 +1,11 @@
+package ConditionalAndLoops.arul;
+
+public class LocicalOperator {
+    int age = 23;
+    int money = 4000;
+        if (age > 21 && money > 500) {
+
+        System.out.println("Welcome");
+    }
+}
+}
