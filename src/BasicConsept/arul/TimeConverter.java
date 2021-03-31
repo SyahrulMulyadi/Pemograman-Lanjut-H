132f8e62ee1ecb9cf912cd9440640ba5147591e6
Syahrul <12050112067@students.uin-suska.ac.id>
2021-03-31 22:08:32 +0700

    Project

--- src/BasicConsept/arul/TimeConverter.java
+++ src/BasicConsept/arul/TimeConverter.java
@@ -1,7 +1,6 @@
 package BasicConsept.arul;
 import java.util.Scanner;
 public class TimeConverter {
-    public static class TimeConverter {
         public static void main(String[] args) {
             Scanner scanner = new Scanner(System.in);
             int days = scanner.nextInt();
