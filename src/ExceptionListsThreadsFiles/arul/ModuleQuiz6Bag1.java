2ed85b2ffe9c7c96d08293fa0325f4951e89475e
Syahrul <12050112067@students.uin-suska.ac.id>
2021-03-31 21:56:13 +0700

    Project

---
+++ src/ExceptionListsThreadsFiles/arul/ModuleQuiz6Bag1.java
@@ -0,0 +1,12 @@
+package ExceptionListsThreadsFiles.arul;
+
+public class ModuleQuiz6Bag1 {
+    // kuis 1
+    void myFunc(int a, int b) {
+        try {
+            System.out.println(a / b);
+        } catch (Exception e) {
+            System.out.println("Eror");
+        }
+    }
+}
