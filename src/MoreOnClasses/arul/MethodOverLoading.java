0394c7a919d809f523f8a195787de0091773f150
Syahrul <12050112067@students.uin-suska.ac.id>
2021-03-31 21:40:47 +0700

    Project

---
+++ src/MoreOnClasses/arul/MethodOverLoading.java
@@ -0,0 +1,17 @@
+package MoreOnClasses.arul;
+
+public class MethodOverLoading {
+    class A {
+        public void doSomething() {
+            System.out.println("A");
+        }
+        public void doSomething(String str){
+            System.out.println(str);
+        }
+    }
+    static class B {
+        public static void main(String[] args) {
+        }
+    }
+
+}
