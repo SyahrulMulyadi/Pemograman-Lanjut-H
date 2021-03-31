1af00e05d38e3231a8bee9ef69de72e699d9daaa
Syahrul <12050112067@students.uin-suska.ac.id>
2021-03-31 21:09:13 +0700

    Project

---
+++ src/Array/arul/ReverseString.java
@@ -0,0 +1,16 @@
+package Array.arul;
+import java.util.Scanner;
+public class ReverseString {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.nextLine();
+        char[] arr = text.toCharArray();
+
+        //your code goes here
+        String rev = "";
+        for(char i:arr){
+            rev = i + rev;
+        }
+        System.out.println(rev);
+    }
+}
