e47bbfcc4fea315a49125b23538a235c57f58dcf
Syahrul <12050112067@students.uin-suska.ac.id>
2021-03-31 21:13:48 +0700

    Project

---
+++ src/ClassesAndObjects/arul/Constructor1.java
@@ -0,0 +1,12 @@
+package ClassesAndObjects.arul;
+
+public class Constructor1 {
+    public static void main(String[] args) {
+        class person{
+            private int age;
+            public person (int myage){
+                age = myage;
+            }
+        }
+    }
+}
