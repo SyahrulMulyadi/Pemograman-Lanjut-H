3f5730efc9cb6516ab48e15ad29089e1bccb931a
Syahrul <12050112067@students.uin-suska.ac.id>
2021-03-31 21:18:56 +0700

    Project

---
+++ src/ClassesAndObjects/arul/GetterAndSetter.java
@@ -0,0 +1,16 @@
+package ClassesAndObjects.arul;
+
+public class GetterAndSetter {
+    public static void main(String[] args) {
+        class A {
+            private int x;
+            public int getX(){
+                return x;
+            }
+            public void setX(int x){
+                this.x = x;
+
+            }
+        }
+    }
+}
