59903424b165b472e71d26e3f0d712a0e7eec5b2
Syahrul <12050112067@students.uin-suska.ac.id>
2021-03-31 20:50:03 +0700

    Project

---
+++ src/ConditionalAndLoops/arul/ifelse.java
@@ -0,0 +1,15 @@
+package ConditionalAndLoops.arul;
+
+public class ifelse {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 5;
+        if ( x > y){
+
+            System.out.println( x );
+        }
+        else{
+            System.out.println( y );
+        }
+    }
+}
