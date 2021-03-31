5569f23b7c990b09c91bc8a5de137156ee665882
Syahrul <12050112067@students.uin-suska.ac.id>
2021-03-31 21:34:02 +0700

    Project

---
+++ src/MoreOnClasses/arul/Encapsulation.java
@@ -0,0 +1,14 @@
+package MoreOnClasses.arul;
+
+public class Encapsulation {
+    private int age;
+
+    public void setAge(int age)
+    {
+        if(age > 0)
+        {
+            this.age = age;
+        }
+    }
+
+}
